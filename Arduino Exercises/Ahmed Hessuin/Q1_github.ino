e1bcdefa2c4bcb194f2344c9b0a110b4e9690189
AhmedHessuin <36261222+AhmedHessuin@users.noreply.github.com>
2018-02-12 19:07:40 +0200

    Add files via upload

---
+++ Arduino Exercises/Ahmed Hessuin/Q1_github.ino
@@ -0,0 +1,17 @@
+
+void setup() {
+  // put your setup code here, to run once:
+pinMode(5,OUTPUT);
+digitalWrite(5,LOW); // intially put pin 5 as low 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(5,HIGH);
+delay(500) // work for 0.5 sec as delay work with mS
+digitalWrite(5,LOW); // set pin 5 low again
+delay(1500); // set off for 1.5 sec 
+// the void loop work as while loop 
+
+
+}
