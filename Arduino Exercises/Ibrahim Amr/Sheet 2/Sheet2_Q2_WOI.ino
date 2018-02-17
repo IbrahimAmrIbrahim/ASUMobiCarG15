1c802246336e1210acaa9dafb47463f0f3921df8
Ibrahim Amr <36275498+IbrahimAmrIbrahim@users.noreply.github.com>
2018-02-17 21:14:33 +0200

    Add files via upload

---
+++ Arduino Exercises/Ibrahim Amr/Sheet 2/Sheet2_Q2_WOI.ino
@@ -0,0 +1,19 @@
+void setup() {
+  pinMode(2 , INPUT_PULLUP);
+  pinMode(3 , INPUT_PULLUP);
+  pinMode(0 , OUTPUT);
+  digitalWrite(0 , LOW);
+}
+int   x = 0;
+void loop() {
+  if ((digitalRead(2) == LOW) && (x < 10000)) {
+    x += 1000;
+  }
+  if ((digitalRead(3) == LOW) && (x > 0)) {
+    x -= 1000;
+  }
+  digitalWrite(0 , HIGH);
+  delay((x / 2));
+  digitalWrite(0 , LOW);
+  delay((x / 2));
+}
