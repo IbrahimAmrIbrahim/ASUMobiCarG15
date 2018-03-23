229fd4ebbb208e2b6d8b2ff718ece1a4e72265d7
AhmedHessuin <36261222+AhmedHessuin@users.noreply.github.com>
2018-03-23 19:54:20 +0200

    Add files via upload

---
+++ Arduino Exercises/Ahmed Hessuin/functions.ino
@@ -0,0 +1,20 @@
+int ping_read(){// read the distance infront of the ping
+  unsigned int  cm ,duration;
+  pinMode(ping_tri, OUTPUT);
+  digitalWrite(ping_tri, LOW);
+  delayMicroseconds(2);
+  digitalWrite(ping_tri, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(ping_tri, LOW);
+  pinMode(ping_ecc, INPUT);
+  duration = pulseIn(ping_ecc, HIGH);
+  cm = duration / 29 / 2 ;
+  delay(2);
+ return cm;
+}
+//====================================//
+void  anti_crush(){// if the distance less than 15 cm STOP!
+  if (ping_read() <= 15 ){
+  Motion(6);
+  }// if
+}// void anti_crush
